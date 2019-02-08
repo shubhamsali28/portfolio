de04099deef260ac710a24ce3910af691e1bbde9
shubhamsali28 <shubhamsali28@gmail.com>
2019-02-07 21:03:43 -0500

    'initial'

---
+++ dist/js/main.js
@@ -0,0 +1,36 @@
+// Selecting DOM elements
+// We use const bcz const elements cannot be changed
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+// Initially the state of menu
+// Since showMenu is going to get changed continuously so we use let
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if(!showMenu)
+    {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+
+        showMenu = true;
+    }
+    else 
+    {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show')); 
+
+        showMenu = false;
+    }
+}
